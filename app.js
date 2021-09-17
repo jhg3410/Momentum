7965a6c7a7ca825720bb23700cc810c10c3470da
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2021-09-17 23:21:43 +0900

    Form Submission

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const loginButton = document.querySelector("#login-form button");
 
 
 function onLoginBtnClick() {
-    console.log("hello", loginInput.value);
+    const username = loginInput.value;
+    console.log(username)
+    // if (username === ""){
+    //     alert("Please write your name");
+    // }
+    // else if(username.length > 15) {
+    //     alert("your name is too long.");
+    // }
 }
 
 loginButton.addEventListener("click",onLoginBtnClick)
