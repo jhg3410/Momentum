399a539d1073600c9e34c17be4996a94b266afb4
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2021-09-18 16:02:07 +0900

    Intervals

--- js/greetings.js
+++ js/greetings.js
@@ -10,12 +10,12 @@ function onLoginSubmit(event) {
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY,username)
-    //greeting.innerText = "Hello " +username;
     paintGreetings(username);
 }
 
 
 function paintGreetings(username){
+    //greeting.innerText = "Hello " +username;
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
