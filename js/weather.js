64a0760f966ed03ec94143227b3e6ef81ceebfff
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2021-09-23 15:32:55 +0900

    Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "e9053f3d3395d508803aed919b3b510d"
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data =>{
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        city.innerText = data.name;
+    });
 }
 
 function onGeoError(){
